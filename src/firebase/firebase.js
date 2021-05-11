f889ed8e0c0a1f53334e0d8e88cc142df07bd515
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-05-11 23:03:01 +0530

    fix: login issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
-import {REACT_APP_FIREBASE_API_ID, REACT_APP_FIREBASE_API_KEY} from '../constants';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: REACT_APP_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "linkedin-clone-930d7.firebaseapp.com",
     projectId: "linkedin-clone-930d7",
     storageBucket: "linkedin-clone-930d7.appspot.com",
     messagingSenderId: "269774521914",
-    appId: REACT_APP_FIREBASE_API_ID
+    appId: process.env.REACT_APP_FIREBASE_API_ID
   };
   // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
