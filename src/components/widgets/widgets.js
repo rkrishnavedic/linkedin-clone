4eeb23386ea9e88c2a9b2c5a9bdc46005e164706
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-06-04 00:17:19 +0530

    .

--- src/components/widgets/widgets.js
+++ src/components/widgets/widgets.js
@@ -26,21 +26,28 @@ const Widgets = ()=>{
 
     return(
         <div className="widgets">
-            <div className="widgets-header">
-                <h3>SyncedIn News</h3>
+            <div className="widgets-music">
+                <iframe style={{borderRadius:'10px'}} src="https://open.spotify.com/embed/playlist/7DABXCuuPXgzogqy3OJIgH" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
             
-            {newsArticle("4.03 lakh new cases, 4092 deaths","Top news • 26,851 readers ")}
-            {newsArticle("Cryptic signals on crypto trade","1d ago • 888 readers ")}
-            {newsArticle("How to spot a bad manager","1d ago • 104,444 readers ")}
-            {newsArticle("Cognizant tries to hire, train, retain","4d ago • 13,612 readers ")}
-            {newsArticle("Kindly, write better emails","1d ago • 2,116 readers")}
-
-            <div className="widgets-show-more">
+            <div className="widgets-item">
+                <div className="widgets-header">
+                    <h3>SyncedIn News</h3>
+                </div>
                 
-                    <h4>Show More </h4> <ArrowDropDownIcon/>
-               
+                {newsArticle("4.03 lakh new cases, 4092 deaths","Top news • 26,851 readers ")}
+                {newsArticle("Cryptic signals on crypto trade","1d ago • 888 readers ")}
+                {newsArticle("How to spot a bad manager","1d ago • 104,444 readers ")}
+                {newsArticle("Cognizant tries to hire, train, retain","4d ago • 13,612 readers ")}
+                {newsArticle("Kindly, write better emails","1d ago • 2,116 readers")}
+
+                <div className="widgets-show-more">
+                    
+                        <h4>Show More </h4> <ArrowDropDownIcon/>
+                
+                </div>
             </div>
+            
         </div>
     )
 }
